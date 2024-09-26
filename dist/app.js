43110bb639e354ddad8d77711ad3dfac3df049f5
Naroci <mircosgiga@hotmail.de>
2024-09-26 16:28:33 +0900

    Project init Test.

---
+++ dist/app.js
@@ -0,0 +1,6 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", () => {
+    const heading = document.createElement("h2");
+    heading.textContent = "Diese Seite wird mit TypeScript erstellt!";
+    document.body.appendChild(heading);
+});
