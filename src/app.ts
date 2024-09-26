5e54a11418eff7013ae2b28adac9adff00c82fa1
Naroci <mircosgiga@hotmail.de>
2024-09-26 16:44:26 +0900

    angular test

--- src/app.ts
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const heading = document.createElement("h2");
-    heading.textContent = "Diese Seite wird mit TypeScript erstellt!";
-    document.body.appendChild(heading);
-});
-
